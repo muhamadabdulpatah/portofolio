98ea94bd14f69cc330c25bd0a7d17165d82f9fce
muhamadabdulpatah <muhamadabdulpatah@gmail.com>
2025-10-24 17:55:23 +0700

    Add Skill;s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = ({ theme, setTheme }) => {
   const navLinks = [
     { name: "Home", href: "#home" },
     { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ];
